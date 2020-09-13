4e5134c56241f4437c41cb282d4acc19339d6b7f
Victoria <victorah@stud.ntnu.no>
2020-09-13 14:23:39 +0200

    Added nessecary fields in the model class for the report and included koiene_report in settings file.

--- backend/ntnui/apps/koiene_report/models.py
+++ backend/ntnui/apps/koiene_report/models.py
@@ -1,8 +1,23 @@
 from django.db import models
-
-# Create your models here.
+from koie_booking.models.booking import BookingModel
 class ReportModel(models.Model):
-    # Every report should contain exactly one koie object
-    koie = models.ForeignKey(KoieModel, null=True, on_delete=models.CASCADE)
-    
-    # Attributes with the rest of the questions will be implemented here
+    # Every report should contain exactly one booking object
+    booking = models.ForeignKey(BookingModel, null = False, on_delete=models.CASCADE)
+    # Date for when report was submitted and saved to database. auto_now makes sure to set the date
+    date_created_at = models.DateField(auto_now= True)
+    # Additional comments on faults, optional
+    comment = models.TextField(blank = True, null = True)
+    # Ranging between 1-3 on amount of wood. 3 means a lot/enough wood
+    STATUS_CHOICES = (
+        (1, "Low"), (2, "Medium"), (3, "High")
+    )
+    wood = models.IntegerField(choices=STATUS_CHOICES, default = 1)
+
+    # Checkbox field. True means that the gas tank is empty. False means that it is full.
+    # The user has to check the checkbox if it is 
+    gas= models.BooleanField(default=False)
+    CHOICES = (
+        ("Y", "Yes"),("N", "No"),("None", "Don't know")
+    )
+    # Radiobutton selection on if the smoke detector worked 
+    smoke_detector = models.CharField(max_length = 4, choices = CHOICES, default= "None")
