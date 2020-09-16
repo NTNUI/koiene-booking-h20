35740ae963c008ef9f2aa9ed0a206a026bd2e665
Victoria <victorah@stud.ntnu.no>
2020-09-16 23:17:45 +0200

    feat: Create API for creating new report for koiene and view a list of all reports in the database.
    BREAKING CHANGE: The routers defined in the urls.py file along with the functions in views.py is tested and works properly now.

--- backend/ntnui/apps/koie_report/urls.py
+++ backend/ntnui/apps/koie_report/urls.py
@@ -1,11 +1,9 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from koie_report.views import ReportViewSet, report_list
+from koie_report.views import ReportViewSet
 
 
 """ Include URL Patterns """
 urlpatterns = [path("reports/create/<int:pk>", ReportViewSet.as_view({'post' : 'create'}),name = 'koie_create'),
-path("reports", report_list,name = 'koie_create'),
-
-]
+path('reports/', ReportViewSet.as_view({'get' : 'list'}), name = "koie_list")]
--- backend/ntnui/apps/koie_report/views.py
+++ backend/ntnui/apps/koie_report/views.py
@@ -30,40 +30,7 @@ class ReportViewSet(viewsets.GenericViewSet, mixins.ListModelMixin
             return Response(serializer.data, status = status.HTTP_201_CREATED)
         return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET', 'POST'])
-def report_list(request, pk):
-    """
-    List all code snippets, or create a new snippet.
-    """
-    if request.method == 'GET':
-        reports =KoieReportModel.objects.all()
-        serializer = ReportSerializer(reports, many=True)
-        return Response(serializer.data)
 
-    elif request.method == 'POST':
-        serializer = ReportSerializer(data=request.data)
-        booking = BookingModel.objects.get(pk = pk)
-        if serializer.is_valid():
-            report = KoieReportModel.objects.create(booking = booking, **serializer.validated_data)
-            report.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-# Gjøre om function based er best
-# class ReportPostViewSet(viewsets.GenericViewSet, mixins.CreateModelMixin):
-#     queryset = KoieReportModel.objects.all()
-#     serializer_class = ReportSerializer
-    # def create(self, request, booking_id):
-    #     # Deserialize 
-    #     serializer = ReportSerializer(data=request.data)
-    #     # Retrieve correct booking instance 
-    #     booking = BookingModel.objects.get(pk = booking_id)
-    #     if serializer.is_valid():
-    #         # Create a new report instance 
-    #         report = KoieReportModel.objects.create(booking = booking, **serializer.validated_data)
-    #         report.save()
-    #         return Response(serializer.data, status = status.HTTP_201_CREATED)
-    #     return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
 
 
 
