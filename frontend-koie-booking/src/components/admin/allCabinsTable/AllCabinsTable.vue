41e4ce43bdcc364f2b9b6df9cdd70cac96bc3bf9
Oskar Størmer <oskar.stormer@gmail.com>
2020-09-21 22:46:48 +0200

    feat: started navigation bar for cabin table
    Dummy buttons and date selector not connected to any functions or data

--- frontend-koie-booking/src/components/admin/allCabinsTable/AllCabinsTable.vue
+++ frontend-koie-booking/src/components/admin/allCabinsTable/AllCabinsTable.vue
@@ -1,6 +1,77 @@
 <template>
   <v-row justify="center">
-    <h4>Her kommer tabell over alle hytter</h4>
+    <v-col>
+      <!-- Calendar selection -->
+      <v-row justify="center">
+        <v-col cols="2">
+          <v-menu
+            v-model="dateMenu"
+            :close-on-content-click="false"
+            :nudge-right="20"
+            :nudgeTop="20"
+            :first-day-of-week="1"
+            transition="scale-transition"
+            offset-y
+            min-width="290px"
+            no-title
+            close-on-change
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-text-field
+                v-model="date"
+                label="Select starting date"
+                prepend-icon="event"
+                readonly
+                v-bind="attrs"
+                v-on="on"
+              ></v-text-field>
+            </template>
+            <v-date-picker v-model="date" @input="dateMenu = false"></v-date-picker>
+          </v-menu>
+        </v-col>
+      </v-row>
+      <!-- Date selction menu bar -->
+      <v-row>
+        <v-col cols="3">
+          <v-row justify="space-around">
+            <v-btn class="navBtn" small height="50px" width="23%">
+              -1 year
+            </v-btn>
+            <v-btn class="navBtn" small height="50px" width="23%">
+              -1 month
+            </v-btn>
+            <v-btn class="navBtn" small height="50px" width="23%">
+              -1 week
+            </v-btn>
+            <v-btn class="navBtn" small height="50px" width="23%">
+              -1 day
+            </v-btn>
+          </v-row>
+        </v-col>
+        <!-- Show dates and day of week here -->
+        <v-col justify="center" cols="6">
+          <v-card class="pa-3" outlined tile>Dates go here</v-card>
+        </v-col>
+        <!-- Buttons for forward in time -->
+        <v-col cols="3">
+          <v-row justify="space-around">
+            <v-btn class="navBtn" small height="50px" width="23%">
+              +1 day
+            </v-btn>
+            <v-btn class="navBtn" small height="50px" width="23%">
+              +1 week
+            </v-btn>
+            <v-btn class="navBtn" small height="50px" width="23%">
+              +1 month
+            </v-btn>
+            <v-btn class="navBtn" small height="50px" width="23%">
+              +1 year
+            </v-btn>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-col>
+    <!-- Buttons for back in time -->
   </v-row>
 </template>
 
@@ -12,4 +83,4 @@ export default Vue.extend({
 });
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped></style>
