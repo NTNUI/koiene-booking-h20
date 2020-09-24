da729ef18cbc991b0ef40c459fb44a08eb9de930
Victoria <victorah@stud.ntnu.no>
2020-09-24 02:14:38 +0200

    fix: merge conflict solved

--- frontend-koie-booking/src/router/index.ts
+++ frontend-koie-booking/src/router/index.ts
@@ -37,7 +37,7 @@ const routes = [
     meta: { requiresAuth: true, requiresAdmin: true }
   },
   {
-    path: '/report',
+    path: '/report/:id',
     name: 'Report',
     component: () => import('@/views/Report.vue'),
     meta: { requiresAuth: false, requiresAdmin: false } //TODO: add id to path
