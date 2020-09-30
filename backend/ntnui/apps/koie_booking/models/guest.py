3610180695a7ff04d4b9f81ed3f13c6b8ebcc23c
Sondre Strand Haltbakk <Sondrehalt@hotmail.com>
2020-09-30 16:03:04 +0200

    remove unnecessary guest model

--- backend/ntnui/apps/koie_booking/models/guest.py
+++ backend/ntnui/apps/koie_booking/models/guest.py
@@ -1,13 +1,4 @@
-from django.db import models
 
-
-class GuestModel(models.Model):
-    email = models.CharField(max_length=40, null=True)
-    phone_number = models.IntegerField(null=True)
-
-    class Meta:
-        verbose_name = "Guest"
-        verbose_name_plural = "Guests"
 
 def get_default_guest():
         return {
