222da42daaceb169b435735fa1cd373818d40fc2
Ellen <ellenzchang@hotmail.com>
2020-10-07 10:06:13 +0200

    refactor: improve renameKey

--- frontend-koie-booking/src/utils/objects.ts
+++ frontend-koie-booking/src/utils/objects.ts
@@ -1,9 +1,8 @@
 export const clone = (object: any) => Object.assign({}, object);
 
 export const renameKey = (object: any, key: string, newKey: string): any => {
-  const clonedObj = clone(object);
-  const targetKey = clonedObj[key];
-  delete clonedObj[key];
-  clonedObj[newKey] = targetKey;
-  return clonedObj;
+  const value = object[key];
+  delete object[key];
+  object[newKey] = value;
+  return object;
 };
