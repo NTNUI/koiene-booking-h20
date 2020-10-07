dd847e84ae7ef77a0c0b079aa7c9cdcb82c0f0c5
Jesper Paulsen <jesper@jesper.no>
2020-10-07 13:50:48 +0200

    test: added tests for admintable

--- frontend-koie-booking/tests/unit/components/admin/allCabinsView/AllCabinsTable.spec.ts
+++ frontend-koie-booking/tests/unit/components/admin/allCabinsView/AllCabinsTable.spec.ts
@@ -9,13 +9,13 @@ import mockAxios from 'jest-mock-axios';
 Vue.use(Vuetify);
 
 // Utilities
-import { createLocalVue, Wrapper, shallowMount, ThisTypedMountOptions, mount } from '@vue/test-utils';
+import { createLocalVue, Wrapper, ThisTypedMountOptions, mount } from '@vue/test-utils';
 
 // Components or views
 import AllCabinsTable from '@/components/admin/allCabinsView/AllCabinsTable.vue';
 import { RootState } from '@/store/types';
 import getKoieData, { MOCK_MOUNT_CABINS_WITH_BOOKINGS, startDate } from '../../../__mocks__/koiene';
-import { addToDate } from '@/utils/dates';
+import { convertAPIBookingToAdminBooking } from '@/store/modules/admin/bookings/helpers';
 
 describe('Component AllCabinsTable.vue', () => {
   let wrapper: Wrapper<any>;
@@ -47,7 +47,76 @@ describe('Component AllCabinsTable.vue', () => {
     mockAxios.reset();
   });
 
-  it('Matches snapshot', async () => {
+  it('Matches snapshot', () => {
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('Cabins are not sorted when table is initially loaded', () => {
+    // Arrange
+    const allCabinsCorrectOrder = getKoieData().map((booking) => convertAPIBookingToAdminBooking(booking));
+    const allCabinsFromComponent = wrapper.vm.allCabinsWithBookings;
+
+    // Assert
+    expect(allCabinsFromComponent).toEqual(allCabinsCorrectOrder);
+  });
+
+  it('Custom sort on cabin name sorts the cabins in descending order', () => {
+    // Arrange
+    const allCabinsFromMock = getKoieData().map((booking) => convertAPIBookingToAdminBooking(booking));
+    const allCabinsFromComponent = wrapper.vm.allCabinsWithBookings;
+
+    // Act
+    const allCabinsCorrectOrder = allCabinsFromMock.sort((cabinA, cabinB) => {
+      return cabinA.slug.localeCompare(cabinB.slug);
+    });
+    const allCabinsFromComponentSorted = wrapper.vm.customSort(allCabinsFromComponent, ['name'], [true]);
+
+    // Assert
+    expect(allCabinsFromComponentSorted).toEqual(allCabinsCorrectOrder);
+  });
+
+  it('Custom sort on cabin name sorts the cabins in ascending order', () => {
+    // Arrange
+    const allCabinsFromMock = getKoieData().map((booking) => convertAPIBookingToAdminBooking(booking));
+    const allCabinsFromComponent = wrapper.vm.allCabinsWithBookings;
+
+    // Act
+    const allCabinsCorrectOrder = allCabinsFromMock.sort((cabinA, cabinB) => {
+      return cabinB.slug.localeCompare(cabinA.slug);
+    });
+    const allCabinsFromComponentSorted = wrapper.vm.customSort(allCabinsFromComponent, ['name'], [false]);
+
+    // Assert
+    expect(allCabinsFromComponentSorted).toEqual(allCabinsCorrectOrder);
+  });
+
+  it('Custom sort on available dates in descending order', () => {
+    // Arrange
+    const allCabinsFromMock = getKoieData().map((booking) => convertAPIBookingToAdminBooking(booking));
+    const allCabinsFromComponent = wrapper.vm.allCabinsWithBookings;
+
+    // Act
+    const allCabinsCorrectOrder = allCabinsFromMock.sort((cabinA, cabinB) => {
+      return cabinA.bedsAvailableInDateRange[startDate] - cabinB.bedsAvailableInDateRange[startDate];
+    });
+    const allCabinsFromComponentSorted = wrapper.vm.customSort(allCabinsFromComponent, [startDate], [true]);
+
+    // Assert
+    expect(allCabinsFromComponentSorted).toEqual(allCabinsCorrectOrder);
+  });
+
+  it('Custom sort on available dates in ascending order', () => {
+    // Arrange
+    const allCabinsFromMock = getKoieData().map((booking) => convertAPIBookingToAdminBooking(booking));
+    const allCabinsFromComponent = wrapper.vm.allCabinsWithBookings;
+
+    // Act
+    const allCabinsCorrectOrder = allCabinsFromMock.sort((cabinA, cabinB) => {
+      return cabinB.bedsAvailableInDateRange[startDate] - cabinA.bedsAvailableInDateRange[startDate];
+    });
+    const allCabinsFromComponentSorted = wrapper.vm.customSort(allCabinsFromComponent, [startDate], [false]);
+
+    // Assert
+    expect(allCabinsFromComponentSorted).toEqual(allCabinsCorrectOrder);
   });
 });
