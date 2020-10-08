1048f4a7f6e15f119dbed03bd6afd426da6171d7
Kåre Fosli Obrestad <kaare@obrestad.net>
2020-10-08 12:15:07 +0200

    test(koie_model): made test more readable, refactor a bit into fixture

--- backend/ntnui/apps/koie_booking/tests/test_koie_model.py
+++ backend/ntnui/apps/koie_booking/tests/test_koie_model.py
@@ -40,34 +40,49 @@ def bookings(koie):
     )
 
 
-@pytest.mark.django_db
-def test_get_beds_available_in_date_range(koie, bookings):
-    date_availability = {}
+@pytest.fixture()
+def expected_availability(koie):
+    """ Builds expected availability of beds, for bookings made in above fixture """
+    expected_availability = {}
     expected = [11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 5, 8, 8, 11]
     for day in date_utils.get_daterange(now(), (now() + timedelta(koie.booking_window))):
-        date_availability[day.strftime("%Y-%m-%d")] = expected.pop()
-    actual = koie.get_beds_available_in_date_range(now(), (now() + timedelta(koie.booking_window)))
-
-    assert actual == date_availability
-    assert len(actual) == koie.booking_window
+        expected_availability[day.strftime("%Y-%m-%d")] = expected.pop()
+    return expected_availability
 
 
 @pytest.mark.django_db
-def test_get_available_beds_by_night(koie, bookings):
+def test_get_beds_available_in_date_range(koie, bookings, expected_availability):
+    """ Koie_model should return correct information about available beds for given date range """
+
+    actual_availability = koie.get_beds_available_in_date_range(
+        now(), (now() + timedelta(koie.booking_window))
+    )
+
     assert koie.number_of_beds == 11
-    date_availability = {}
-    expected = [11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 5, 8, 8, 11]
-    for day in date_utils.get_daterange(now(), (now() + timedelta(koie.booking_window))):
-        date_availability[day.strftime("%Y-%m-%d")] = expected.pop()
-    assert koie.get_beds_available_in_booking_window() == date_availability
-    assert len(koie.get_beds_available_in_booking_window()) == koie.booking_window
+    assert actual_availability == expected_availability
+    assert len(actual_availability) == koie.booking_window
+
+
+@pytest.mark.django_db
+def test_get_available_beds_in_booking_window(koie, bookings, expected_availability):
+    """ Koie_model should return correct information about available beds in its booking window """
+
+    actual_availability = koie.get_beds_available_in_booking_window()
+
+    assert koie.number_of_beds == 11
+    assert actual_availability == expected_availability
+    assert len(actual_availability) == koie.booking_window
 
 
 @pytest.mark.django_db
 def test_get_reserved_bookings_returns_correct_amount_of_guests(koie, bookings):
+    """ Helper function get_reserved_bookings in koie_model should return correct amount of beds reserved """
+
     assert koie.get_number_of_reserved_beds(date.today() + timedelta(3)) == 3
 
 
 @pytest.mark.django_db
 def test_get_paid_bookings_returns_correct_amount_of_bookings(koie, bookings):
+    """ Helper function get_paid_bookings in koie_model should return correct amount of beds that have been paid for """
+
     assert koie.get_number_of_sold_beds(date.today() + timedelta(3)) == 3
