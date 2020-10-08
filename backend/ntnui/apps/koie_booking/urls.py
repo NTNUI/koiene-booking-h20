8309a4d15ca2ce727b6e6af8845864fac2f71887
Kåre Fosli Obrestad <kaare@obrestad.net>
2020-10-08 12:10:48 +0200

    fix(urls.py): added missing import

--- backend/ntnui/apps/koie_booking/urls.py
+++ backend/ntnui/apps/koie_booking/urls.py
@@ -1,6 +1,7 @@
 from django.urls import include, path
 from rest_framework import routers
 
+from koie_booking.views.booking import BookingViewSet
 from koie_booking.views.koie import KoieViewSet
 from koie_booking.views.koie_dashboard import KoieDashboardViewSet
 
