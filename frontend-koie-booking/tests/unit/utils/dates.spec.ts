aa43dd1cf380a4a63fa269c9030373587d072224
Oskar Størmer <oskar.stormer@gmail.com>
2020-10-10 12:57:50 +0200

    feat: Unit tests for addToDate and formatDate in utils/dates.ts done

--- frontend-koie-booking/tests/unit/utils/dates.spec.ts
+++ frontend-koie-booking/tests/unit/utils/dates.spec.ts
@@ -1,5 +1,6 @@
 import dayjs from 'dayjs';
 import { addToDate, formatDate } from '@/utils/dates';
+import { Dictionary } from 'vue-router/types/router';
 
 require('dayjs/locale/nb');
 dayjs.locale('nb');
@@ -26,10 +27,32 @@ describe('util Function addToDate', () => {
     { startDate: '2020-01-01', howMany: 2, what: 'year' },
     { startDate: '2020-01-01', howMany: 50, what: 'day' },
   ];
-
-  const expected = ['2020-01-04', '2019-12-28', '2020-02-29', '2022-01-01', '2020-02-20'];
-  // Act/Assert
+  const expected: string[] = ['2020-01-04', '2019-12-11', '2020-02-29', '2022-01-01', '2020-02-20'];
+  // Act and Assert
   for (let i = 0; i < inputs.length; i++) {
     run(inputs[i].startDate, inputs[i].howMany, inputs[i].what, expected[i]);
+  }
+});
+
+describe('util Function formatDate', () => {
+  function run(dateISO: string, formatString: string, expected: string) {
+    it('formats the date ' + dateISO + ' to the format ' + formatString, () => {
+      // Act
+      const formattedDateString = formatDate(dateISO, formatString);
+      // Assert
+      expect(formattedDateString).toEqual(expected);
+    });
+  }
+  // Arrange
+  const inputs: Array<Dictionary<string>> = [
+    { dateISO: '2020-01-01', formatString: 'D. MMMM YYYY' },
+    { dateISO: '2020-02-24', formatString: 'MM/DD/YY' },
+    { dateISO: '2020-10-10', formatString: 'dddd' },
+    { dateISO: '2020-10-12', formatString: 'dd D.M.YYYY' },
+  ];
+  const expected: string[] = ['1. januar 2020', '02/24/20', 'lørdag', 'ma 12.10.2020']
+  // Act and Assert
+  for (let i = 0; i < inputs.length; i++) {
+    run(inputs[i].dateISO, inputs[i].formatString, expected[i]);
   }
 });
