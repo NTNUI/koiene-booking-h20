79a5a83ebf5263f80c1c6587f69d47b39f963714
Sondre Strand Haltbakk <Sondrehalt@hotmail.com>
2020-10-10 15:31:50 +0200

    feat: create base functionality

---
+++ backend/ntnui/apps/koie_booking/reminder/__init__.py
@@ -0,0 +1,9 @@
+import datetime
+from koie_booking.models.booking import BookingModel
+from koie_booking.utils import mail_utils
+
+def reminder_job():
+    queries = BookingModel.objects.filter(departure_date=datetime.date.today())
+    for booking in queries:
+        mail_utils.send_reminder_email(booking)
+    
