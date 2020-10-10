ff6d9318e3c76149d34ba4f8d6783dbb41a3e7b3
Jesper Paulsen <jesper@jesper.no>
2020-10-10 11:06:53 +0200

    test: corrected jests coverage paths

--- frontend-koie-booking/jest.config.js
+++ frontend-koie-booking/jest.config.js
@@ -2,5 +2,10 @@ module.exports = {
   preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
   setupFiles: [],
   collectCoverage: true,
-  collectCoverageFrom: ['src/components/admin/*', 'src/components/report/*'],
+  collectCoverageFrom: [
+    'src/components/admin/**',
+    'src/components/report/**',
+    'src/views/Admin.vue',
+    'src/views/Report.vue',
+  ],
 };
