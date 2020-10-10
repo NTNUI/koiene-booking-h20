26bb1cc48b4e7fbbbbcd982887c4c77f9e84e6a5
Oskar Størmer <oskar.stormer@gmail.com>
2020-10-10 14:17:07 +0200

    fix: Changed parameter name to clarify meaning, removed comments
    what -> timeUnit

--- frontend-koie-booking/tests/unit/utils/dates.spec.ts
+++ frontend-koie-booking/tests/unit/utils/dates.spec.ts
@@ -8,42 +8,40 @@ dayjs.locale('nb');
 describe('util Function addToDate', () => {
   function run(startDate: string, howMany: number, what: dayjs.OpUnitType, expected: string): void {
     it('adds ' + howMany + ' ' + what + ' to date ' + startDate, () => {
-      // Act
       const resultDate = addToDate(startDate, howMany, what);
-      // Assert
+
       expect(resultDate).toEqual(expected);
     });
   }
-  // Arrange
+
   interface addToDateTestInput {
     startDate: string;
     howMany: number;
-    what: dayjs.OpUnitType;
+    timeUnit: dayjs.OpUnitType;
   }
   const inputs: Array<addToDateTestInput> = [
-    { startDate: '2020-01-01', howMany: 3, what: 'day' },
-    { startDate: '2020-01-01', howMany: -3, what: 'week' },
-    { startDate: '2020-01-31', howMany: 1, what: 'month' },
-    { startDate: '2020-01-01', howMany: 2, what: 'year' },
-    { startDate: '2020-01-01', howMany: 50, what: 'day' },
+    { startDate: '2020-01-01', howMany: 3, timeUnit: 'day' },
+    { startDate: '2020-01-01', howMany: -3, timeUnit: 'week' },
+    { startDate: '2020-01-31', howMany: 1, timeUnit: 'month' },
+    { startDate: '2020-01-01', howMany: 2, timeUnit: 'year' },
+    { startDate: '2020-01-01', howMany: 50, timeUnit: 'day' },
   ];
   const expected: string[] = ['2020-01-04', '2019-12-11', '2020-02-29', '2022-01-01', '2020-02-20'];
-  // Act and Assert
+
   for (let i = 0; i < inputs.length; i++) {
-    run(inputs[i].startDate, inputs[i].howMany, inputs[i].what, expected[i]);
+    run(inputs[i].startDate, inputs[i].howMany, inputs[i].timeUnit, expected[i]);
   }
 });
 
 describe('util Function formatDate', () => {
   function run(dateISO: string, formatString: string, expected: string) {
     it('formats the date ' + dateISO + ' to the format ' + formatString, () => {
-      // Act
       const formattedDateString = formatDate(dateISO, formatString);
-      // Assert
+
       expect(formattedDateString).toEqual(expected);
     });
   }
-  // Arrange
+
   const inputs: Array<Dictionary<string>> = [
     { dateISO: '2020-01-01', formatString: 'D. MMMM YYYY' },
     { dateISO: '2020-02-24', formatString: 'MM/DD/YY' },
@@ -51,7 +49,7 @@ describe('util Function formatDate', () => {
     { dateISO: '2020-10-12', formatString: 'dd D.M.YYYY' },
   ];
   const expected: string[] = ['1. januar 2020', '02/24/20', 'lørdag', 'ma 12.10.2020']
-  // Act and Assert
+
   for (let i = 0; i < inputs.length; i++) {
     run(inputs[i].dateISO, inputs[i].formatString, expected[i]);
   }
