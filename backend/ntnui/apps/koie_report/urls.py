7ea197f01662bafc28ecd66d52af99bc105fdea3
Kåre Fosli Obrestad <kaare@obrestad.net>
2020-10-12 18:30:11 +0200

    feat(urls.py): added endpoint for getting reports on koie slug

--- backend/ntnui/apps/koie_report/urls.py
+++ backend/ntnui/apps/koie_report/urls.py
@@ -6,4 +6,5 @@ from koie_report.views import ReportViewSet
 urlpatterns = [
     path("reports/<int:pk>", ReportViewSet.as_view({"post": "create"}), name="koie_create"),
     path("reports/", ReportViewSet.as_view({"get": "list"}), name="koie_list"),
+    path("reports/<slug:slug>", ReportViewSet.as_view({"get": "reports_filter_list"})),
 ]
