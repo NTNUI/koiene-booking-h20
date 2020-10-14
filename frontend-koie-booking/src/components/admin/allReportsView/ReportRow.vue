4a79cb8255789a996b3ea904b310819737da9318
Oskar <oskar.stormer@gmail.com>
2020-10-14 12:54:43 +0200

    admin-reports-table (#40)
    * feat: header for AllReportsTable
    
    * test: Snapshot test for AllReportsTable Component
    
    * test: added snapshot
    
    Co-authored-by: Jesper Paulsen <jesper@jesper.no>

---
+++ frontend-koie-booking/src/components/admin/allReportsView/ReportRow.vue
@@ -0,0 +1,54 @@
+<template>
+  <tr>
+    <td>
+      <!-- Koie -->
+    </td>
+    <td>
+      <!-- Tidsmerke -->
+    </td>
+    <td>
+      <!-- Turdato -->
+    </td>
+    <td>
+      <!-- Gassbestand -->
+    </td>
+    <td>
+      <!-- Vedstokker -->
+    </td>
+    <td>
+      <!-- Opphugd ved -->
+    </td>
+    <td>
+      <!-- Båt -->
+    </td>
+    <td>
+      <!-- Kano -->
+    </td>
+    <td>
+      <!-- Redningsvest -->
+    </td>
+    <td>
+      <!-- Røykvarsler -->
+    </td>
+    <td>
+      <!-- Utstyr -->
+    </td>
+    <td>
+      <!-- Andre utstyrsfeil -->
+    </td>
+    <td>
+      <!-- Tilbakemelding -->
+    </td>
+  </tr>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'ReportRow',
+  components: {},
+});
+</script>
+
+<style scoped></style>
