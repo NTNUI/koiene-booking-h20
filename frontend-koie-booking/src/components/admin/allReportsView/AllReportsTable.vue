12e5226b6c3b88e2fb5ffe133dc03a215de763af
Oskar Størmer <oskar.stormer@gmail.com>
2020-10-14 10:41:55 +0200

    feat: header for AllReportsTable

---
+++ frontend-koie-booking/src/components/admin/allReportsView/AllReportsTable.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-data-table
+    style="width: 100%"
+    :headers="headers"
+    :items="allCabinsWithBookings"
+    :items-per-page="24"
+    :hide-default-footer="true"
+    loading-text="Henter inn rapporter"
+    no-data-text="Det skjedde noe feil da vi prøvde å hente rapporter"
+  >
+  </v-data-table>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import reportTableHeaders from '@/components/admin/allReportsView/ReportTableHeaders.ts';
+
+export default Vue.extend({
+  name: 'AllReportsTable',
+  components: {},
+  data() {
+    return {
+      headers: reportTableHeaders,
+    };
+  },
+  computed: {},
+  methods: {},
+});
+</script>
+
+<style scoped></style>
--- frontend-koie-booking/src/components/admin/allReportsView/AllReportsView.vue
+++ frontend-koie-booking/src/components/admin/allReportsView/AllReportsView.vue
@@ -4,16 +4,20 @@
     <v-row justify="center">
       <ReportsFilter />
     </v-row>
+    <v-row>
+      <AllReportsTable />
+    </v-row>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import ReportsFilter from '@/components/admin/allReportsView/ReportsFilter.vue';
+import AllReportsTable from '@/components/admin/allReportsView/AllReportsTable.vue';
 
 export default Vue.extend({
   name: 'Reports',
-  components: { ReportsFilter },
+  components: { ReportsFilter, AllReportsTable },
 });
 </script>
 
