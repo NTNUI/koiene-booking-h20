493c79a0e388d0c921dc307c0996b9e0d696fbd5
Kåre Fosli Obrestad <kaare@obrestad.net>
2020-10-15 12:44:26 +0200

    refactor(report view): removed unused import, action decorator

--- backend/ntnui/apps/koie_report/views.py
+++ backend/ntnui/apps/koie_report/views.py
@@ -1,5 +1,4 @@
 from rest_framework import mixins, viewsets
-from rest_framework.decorators import action
 from rest_framework.response import Response
 
 from koie_booking.models.booking import BookingModel
