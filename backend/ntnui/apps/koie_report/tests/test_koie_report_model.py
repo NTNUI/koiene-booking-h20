d220cf8cb9137f918c5d3848aae440e6910ca145
Kåre Fosli Obrestad <37066508+Kobrestad@users.noreply.github.com>
2020-10-15 14:16:49 +0200

    feat: admin get koiereports on koie slug endpoint (#41)
    * feat(urls.py): added endpoint for getting reports on koie slug
    
    * feat(views.py): added custom action in viewset for getting reports with filter on koie_slug
    
    * feat(report_serializer.py): added new serializer to show equipment status sorted by answer
    
    * feat(models.py): added method for counting answers of eqipment_status and grouping them
    
    * feat(views.py): fixed permissions
    
    * feat(validation): added authentication check, as well as returning 404 if no results are given
    
    * test(report_tests): added tests for filter_serializer and model method. also fixed broken test in existing integration test, and added test for filter-functionality
    
    * reformat(views.py): fixed import order to make isort happy
    
    * style(koie_report, models and test_report + whitelist.txt): added word to whitelist, made variable name more readable and removed print statement
    
    * refactor(get_equipment_status): removed accumulator variable, and wrote it in a cleaner style
    
    * refactor(report_tests): removed unused fixtures(factories).
    
    * test(test_report.py):  added test that tests permissions of board member from other groups than koiene.
    
    * refactor(test_report): removed print statement that was picked up by backend-ci.
    
    * refactor!(urls, view, test report): refactored to use list() instead of action decorator. Removed generic list function.
    
    * refactor(report view): removed unused import, action decorator

---
+++ backend/ntnui/apps/koie_report/tests/test_koie_report_model.py
@@ -0,0 +1,28 @@
+import pytest
+
+from groups.factories.group_factory import GroupFactory
+from koie_report.factories.report_factory import ReportFactory
+
+
+@pytest.fixture(autouse=True)
+def koie_group():
+    return GroupFactory(name="Koiene")
+
+
+@pytest.fixture()
+def koie_report():
+    return ReportFactory(axe=1, hammer=1, spade=2)
+
+
+@pytest.fixture
+def expected_data():
+    return {"0": 12, "1": 2, "2": 1}
+
+
+@pytest.mark.django_db
+def test_get_sorted_equipment_status_returns_correct_count(koie_report, expected_data):
+    """
+    Tests helper function of report_model that counts equipment
+    status and groups them into their respective answers
+    """
+    assert koie_report.get_sorted_equipment_status() == expected_data
--- backend/ntnui/apps/koie_report/views.py
+++ backend/ntnui/apps/koie_report/views.py
@@ -2,9 +2,10 @@ from rest_framework import mixins, viewsets
 from rest_framework.response import Response
 
 from koie_booking.models.booking import BookingModel
+from koie_booking.models.koie import KoieModel
 from koie_report.models import KoieReportModel
 from koie_report.permissions import IsKoieAdmin
-from koie_report.report_serializer import ReportSerializer
+from koie_report.report_serializer import FilteredReportSerializer, ReportSerializer
 
 from django.utils.timezone import now
 from django.utils.translation import gettext as _
@@ -12,24 +13,39 @@ from django.utils.translation import gettext as _
 
 class ReportViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     """
-    ViewSet for getting all reports and post a koie report.
+    ViewSet for getting reports from koie_slug and post a koie report.
     """
 
     queryset = KoieReportModel.objects.all()
     serializer_class = ReportSerializer
     permission_classes = [IsKoieAdmin]
 
-    def list(self, request):
+    def list(self, request, slug):
+        """ Lists all reports for given koie_slug """
         if IsKoieAdmin.has_object_permission(request.user, request=request, view=self):
-            reports = KoieReportModel.objects.all()
-            serializer = ReportSerializer(reports, context={"request": request}, many=True)
-            return Response(serializer.data)
+            try:
+                koie = KoieModel.objects.get(slug=slug)
+                reports = KoieReportModel.objects.filter(booking__koie=koie)
+            except KoieModel.DoesNotExist:
+                return Response(
+                    {"detail": _(f"Koie with specified slug: {slug}, not found.")}, status=404
+                )
+
+            serializer = FilteredReportSerializer(reports, context={"request": request}, many=True)
+            if len(serializer.data) == 0:
+                return Response(
+                    {"detail": _(f"No reports found for given koie_slug: {slug}.")}, status=404
+                )
+
+            return Response({"reports": serializer.data}, status=200)
+
         else:
             return Response(
                 {"detail": _("You must be a koie admin to access report information.")}, status=403
             )
 
     def create(self, request, pk):
+        """ Create new report, {id} is booking_id for the booking the report is connected to """
         serializer = ReportSerializer(data=request.data)
         booking = BookingModel.objects.get(pk=pk)
         if serializer.is_valid():
@@ -37,8 +53,9 @@ class ReportViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
                 booking=booking, date_created_at=now(), **serializer.validated_data
             )
             report.save()
+
             return Response({"detail": _("Report were successfully created.")}, status=201)
         return Response(
-            {"detail": _("You have passed in invalid data. Make sure to pass invalid data.")},
+            {"detail": _("You have passed in invalid data. Make sure to pass valid data.")},
             status=400,
         )
