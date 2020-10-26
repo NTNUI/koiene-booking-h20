20ad538c233e2309151360875af66d73f28492f1
Kåre Fosli Obrestad <kaare@obrestad.net>
2020-10-26 10:47:02 +0100

    fix(urls.py): report endpoint can accept string with æøå.

--- backend/ntnui/apps/koie_report/urls.py
+++ backend/ntnui/apps/koie_report/urls.py
@@ -7,5 +7,5 @@ from koie_report.views import ReportViewSet
 urlpatterns = [
     path("reports/", ReportViewSet.as_view({"get": "list"}), name="koie_report_list"),
     path("reports/<uuid:uuid>", ReportViewSet.as_view({"post": "create"}), name="koie_create"),
-    path("reports/<slug:slug>", ReportViewSet.as_view({"get": "reports_filter_list"})),
+    path("reports/<str:slug>", ReportViewSet.as_view({"get": "reports_filter_list"})),
 ]
