54a13cce6e58b04b040f6e398d3d6d12cfe45fb9
Kåre Fosli Obrestad <37066508+Kobrestad@users.noreply.github.com>
2020-10-29 14:13:52 +0100

    Authorization for availability endpoint (#51)
    * feat(koie_dashboard, test_availability): authorization is now in place for koie-availability endpoint. corresponding tests are also in place.
    
    * refactor: removed print-statement

--- backend/ntnui/apps/koie_booking/views/koie_dashboard.py
+++ backend/ntnui/apps/koie_booking/views/koie_dashboard.py
@@ -8,8 +8,10 @@ from koie_booking.serializers.all_koier_detailed import (
     KoierDetailedRangeSerializer,
     KoierDetailedSerializer,
 )
+from koie_report.permissions import IsKoieAdmin
 
 from django.utils.timezone import now, timedelta
+from django.utils.translation import gettext as _
 
 
 class KoieDashboardViewSet(
@@ -30,30 +32,42 @@ class KoieDashboardViewSet(
             Dates are supplied as query_parameters {from_date} and {to_date}.
             Format: "YYYY-MM-DD"
         """
-        from_date = request.query_params.get("from_date")
-        if not from_date:
-            from_date = now().date().isoformat()
+        if IsKoieAdmin.has_object_permission(request.user, request=request, view=self):
+            from_date = request.query_params.get("from_date")
+            if not from_date:
+                from_date = now().date().isoformat()
 
-        to_date = request.query_params.get("to_date")
-        if not to_date:
-            to_date = (now().date() + timedelta(days=constants.DEFAULT_BOOKING_WINDOW)).isoformat()
+            to_date = request.query_params.get("to_date")
+            if not to_date:
+                to_date = (
+                    now().date() + timedelta(days=constants.DEFAULT_BOOKING_WINDOW)
+                ).isoformat()
 
-        serializer = KoierDetailedRangeSerializer(
-            self.queryset,
-            context={"request": request, "from_date": from_date, "to_date": to_date},
-            many=True,
-        )
-        return Response({"koier": serializer.data})
+            serializer = KoierDetailedRangeSerializer(
+                self.queryset,
+                context={"request": request, "from_date": from_date, "to_date": to_date},
+                many=True,
+            )
+            return Response({"koier": serializer.data})
+        else:
+            return Response(
+                {"detail": _("You must be a koie admin to access koie availability.")}, status=403
+            )
 
     def list(self, request):
         """ List dashboard details of all koier from current date and given number of days forward
             {days} is supplied as query_parameter
         """
-        days = request.query_params.get("days")
-        if not days:
-            days = constants.DEFAULT_BOOKING_WINDOW
+        if IsKoieAdmin.has_object_permission(request.user, request=request, view=self):
+            days = request.query_params.get("days")
+            if not days:
+                days = constants.DEFAULT_BOOKING_WINDOW
 
-        serializer = KoierDetailedSerializer(
-            self.queryset, context={"request": request, "days": days}, many=True
-        )
-        return Response({"koier": serializer.data})
+            serializer = KoierDetailedSerializer(
+                self.queryset, context={"request": request, "days": days}, many=True
+            )
+            return Response({"koier": serializer.data})
+        else:
+            return Response(
+                {"detail": _("You must be a koie admin to access koie availability.")}, status=403
+            )
--- backend/ntnui/tests/test_koie_availability.py
+++ backend/ntnui/tests/test_koie_availability.py
@@ -1,6 +1,9 @@
 import pytest
 from rest_framework.test import APIRequestFactory, force_authenticate
 
+from accounts.factories.user_factory import UserFactory
+from groups.factories.group_factory import GroupFactory
+from groups.factories.membership_factory import BoardMembershipFactory
 from koie_booking.factories.koie_factory import DescriptionFactory, KoieFactory, LocationFactory
 from koie_booking.models.koie import KoieModel
 from koie_booking.views.koie_dashboard import KoieDashboardViewSet
@@ -11,6 +14,31 @@ from django.utils.timezone import now, timedelta
 @pytest.fixture()
 def request_factory():
     return APIRequestFactory()
+
+
+@pytest.fixture
+def user(autouse=True):
+    return UserFactory()
+
+
+@pytest.fixture(autouse=True)
+def koie_group():
+    return GroupFactory(name="Koiene")
+
+
+@pytest.fixture
+def other_group():
+    return GroupFactory()
+
+
+@pytest.fixture
+def board_membership(user, koie_group):
+    return BoardMembershipFactory(member=user, group=koie_group)
+
+
+@pytest.fixture
+def other_board_membership(user, other_group):
+    return BoardMembershipFactory(member=user, group=other_group)
 
 
 @pytest.fixture()
@@ -29,7 +57,7 @@ def location():
 
 
 def get_response(request, user=None, range=None):
-    force_authenticate(request, user=user)
+    force_authenticate(request=request, user=user)
 
     if range:
         view = KoieDashboardViewSet.as_view({"get": "range"})
@@ -40,11 +68,11 @@ def get_response(request, user=None, range=None):
 
 
 @pytest.mark.django_db
-def test_list_koie_availability_for_next_days(request_factory, koie):
+def test_list_koie_availability_for_next_days(request_factory, user, board_membership, koie):
     """ An admin should be able to list koie availability for the next couple of days """
 
     request = request_factory.get(f"/koie/availability/?days=4")
-    response = get_response(request=request)
+    response = get_response(request=request, user=user)
 
     bed_availability = response.data["koier"][0]["beds_available_in_date_range"]
 
@@ -54,7 +82,19 @@ def test_list_koie_availability_for_next_days(request_factory, koie):
 
 
 @pytest.mark.django_db
-def test_list_koie_availability_from_date_range(request_factory, koie):
+def test_list_koie_availability_for_next_days_unauthorized(
+    request_factory, user, other_board_membership, koie
+):
+    """ A non-admin should not be able to list koie availability for the next couple of days """
+
+    request = request_factory.get(f"/koie/availability/?days=4")
+    response = get_response(request=request, user=user)
+
+    assert response.status_code == 403
+
+
+@pytest.mark.django_db
+def test_list_koie_availability_from_date_range(request_factory, user, board_membership, koie):
     """ An admin should be able to list koie availability for given date range """
 
     from_date = now().date().isoformat()
@@ -63,9 +103,26 @@ def test_list_koie_availability_from_date_range(request_factory, koie):
     request = request_factory.get(
         f"/koie/availability/range/?from_date={from_date}&to_date={to_date}"
     )
-    response = get_response(request=request)
+    response = get_response(request=request, user=user)
     bed_availability = response.data["koier"][0]["beds_available_in_date_range"]
 
     assert len(response.data["koier"]) == KoieModel.objects.count()
     assert len(bed_availability) == 14
     assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_list_koie_availability_from_date_range_unauthorized(
+    request_factory, user, other_board_membership, koie
+):
+    """ A non-admin should not be able to list koie availability for given date range """
+
+    from_date = now().date().isoformat()
+    to_date = (now().date() + timedelta(days=14)).isoformat()
+
+    request = request_factory.get(
+        f"/koie/availability/range/?from_date={from_date}&to_date={to_date}"
+    )
+    response = get_response(request=request, user=user)
+
+    assert response.status_code == 403
