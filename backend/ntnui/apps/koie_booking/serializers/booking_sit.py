da5d9791328daf3e6e1c60a501ee5d299f95c4f9
Kåre Fosli Obrestad <kaare@obrestad.net>
2020-11-03 15:12:48 +0100

    feat(urls, seralizers, views): initial working version for changing only key_status field using keystatus enum.

---
+++ backend/ntnui/apps/koie_booking/serializers/booking_sit.py
@@ -0,0 +1,101 @@
+from rest_framework import serializers
+
+from koie_booking.models.booking import BookingModel
+from koie_booking.models.koie import KoieModel
+from koie_booking.utils import date_utils
+from ntnui.enums import KeyStatus
+from enumchoicefield import EnumChoiceField
+
+from django.utils.timezone import now, timedelta
+from django.utils.translation import gettext as _
+
+
+class BookingSitSerializer(serializers.ModelSerializer):
+    user = serializers.IntegerField(source="user.ntnui_no", read_only=True)
+    koie = serializers.SlugRelatedField(
+        many=False, slug_field="slug", queryset=KoieModel.objects.all()
+    )
+    booking_transaction_id = serializers.SerializerMethodField()
+    price = serializers.SerializerMethodField()
+    key_status = EnumChoiceField(enum_class=KeyStatus)
+
+    def __init__(self, *args, **kwargs):
+        kwargs["partial"] = True
+        super(BookingSitSerializer, self).__init__(*args, **kwargs)
+
+    class Meta:
+        model = BookingModel
+
+        fields = [
+            "id",
+            "price",
+            "koie",
+            "user",
+            "uuid",
+            "booking_transaction_id",
+            "arrival_date",
+            "departure_date",
+            "contact_email",
+            "guests_member",
+            "guests_not_member",
+            "paid",
+            "created",
+            "key_status",
+        ]
+        read_only_fields = (
+            "id",
+            "price",
+            "koie",
+            "user",
+            "uuid",
+            "booking_transaction_id",
+            "arrival_date",
+            "departure_date",
+            "contact_email",
+            "guests_member",
+            "guests_not_member",
+            "paid",
+            "created",
+        )
+
+    def get_booking_transaction_id(self, obj):
+        """Tries to get the transaction ID, if no payment object exists it returns
+        "Has no payment obj"""
+        try:
+            return obj.get_transaction_id()
+        except AttributeError:
+            return "Has no payment object"
+
+    def get_price(self, obj):
+        return obj.get_total_price()
+
+    def validate_has_key_status(self, data):
+        """Checks that the request contains valid key_status """
+        if not (
+            (data["key_status"] == KeyStatus.not_picked_up)
+            or (data["key_status"] == KeyStatus.picked_up)
+            or (data["key_status"] == KeyStatus.delivered)
+        ):
+            raise serializers.ValidationError(
+                detail={
+                    "detail": _(
+                        "You cannot change key_status without supplying valid key_status in request"
+                    ),
+                    "data": _(data["key_status"]),
+                }
+            )
+
+    def validate(self, data):
+        self.validate_has_key_status(data)
+        return data
+
+    def update(self, instance, validated_data):
+        key_status = validated_data.pop("key_status")
+        if (key_status is not None) and (
+            (key_status == KeyStatus.not_picked_up)
+            or (key_status == KeyStatus.picked_up)
+            or (key_status == KeyStatus.delivered)
+        ):
+            instance.key_status = key_status
+        instance.save()
+        return super(BookingSitSerializer, self).update(instance, validated_data)
---
+++ backend/ntnui/apps/koie_booking/views/booking_sit.py
@@ -0,0 +1,61 @@
+from django.core.exceptions import ValidationError, ObjectDoesNotExist
+from rest_framework import mixins, viewsets
+from rest_framework.response import Response
+
+from koie_booking.models.booking import BookingModel
+from koie_booking.serializers.booking_sit import BookingSitSerializer
+from ntnui.enums import KeyStatus
+
+from django.utils.translation import gettext as _
+
+
+class BookingSitViewSet(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = BookingModel.objects.all()
+    serializer_class = BookingSitSerializer
+    lookup_field = "uuid"
+
+    def list(self, request):
+        bookings = BookingModel.objects.all()
+        serializer = BookingSitSerializer(bookings, context={"request": request}, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, uuid):
+        try:
+            booking = self.queryset.get(uuid=uuid)
+            serializer = BookingSitSerializer(booking, context={"request": request})
+            return Response({"booking": serializer.data})
+        except BookingModel.DoesNotExist:
+            return Response({"detail": _("Booking not found.")}, status=404)
+
+    def update(self, request, *args, **kwargs):
+        """ Returns a response saying that the method is not allowed. """
+
+        return Response({"detail": "Not yet implemented use PATCH"}, status=400)
+
+    def partial_update(self, request, uuid=None):
+        """ Changes field: key_status. """
+
+        try:
+            self.check_object_permissions(self.request, self.get_object())
+            serializer = BookingSitSerializer(
+                self.get_object(),
+                context={"request": request},
+                many=False,
+                data=request.data,
+                partial=True,
+            )
+            if serializer.is_valid():
+                self.perform_update(serializer)
+                return Response({"detail": _("Changes were made")})
+            else:
+                return Response({"detail": serializer.errors}, status=400)
+        except BookingModel.DoesNotExist:
+            return Response({"detail": "Could not find group"}, status=400)
+        except ObjectDoesNotExist:
+            return Response({"detail": "Could not find object"}, status=400)
+
