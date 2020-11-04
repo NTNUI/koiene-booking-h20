838862afff82b1bd21e983613946ac3b106b15d4
Kåre Fosli Obrestad <kaare@obrestad.net>
2020-11-04 10:38:29 +0100

    test(sit_serializer): finalized serializer testing

--- backend/ntnui/apps/koie_booking/tests/test_booking_sit_serializer.py
+++ backend/ntnui/apps/koie_booking/tests/test_booking_sit_serializer.py
@@ -5,7 +5,7 @@ from accounts.factories.user_factory import UserFactory
 from groups.factories.group_factory import GroupFactory
 from koie_booking.factories.booking_factory import BookingFactory
 from koie_booking.factories.koie_factory import KoieFactory
-from koie_booking.serializers.booking import BookingSerializer
+from koie_booking.serializers.booking_sit import BookingSitSerializer
 
 from django.utils.timezone import now, timedelta
 
@@ -17,7 +17,7 @@ def user():
 
 @pytest.fixture(autouse=True)
 def koie_group():
-    return GroupFactory(name="SiT")
+    return GroupFactory(name="Koiene")
 
 
 @pytest.fixture
@@ -27,12 +27,24 @@ def booking():
 
 @pytest.fixture
 def serializer(booking):
-    return BookingSerializer(instance=booking)
+    return BookingSitSerializer(instance=booking)
 
 
 @pytest.fixture
 def koie():
     return KoieFactory()
+
+
+@pytest.fixture
+def data(koie):
+    return {
+        "koie": koie.slug,
+        "arrival_date": now().date(),
+        "departure_date": now().date() + timedelta(days=1),
+        "guests_member": 1,
+        "guests_not_member": 0,
+        "key_status": "not_picked_up",
+    }
 
 
 @pytest.mark.django_db
@@ -58,32 +70,18 @@ def test_contains_expected_fields(serializer):
 
 
 @pytest.mark.django_db
-@pytest.mark.parametrize(
-    "guests_member, guests_not_member", [(2, -1), (-1, 2), (-1, 0), (0, -1), (0, 0)]
-)
-def test_validate_negative_number_of_guests_raises_error(data, guests_member, guests_not_member):
-    data["guests_member"] = guests_member
-    data["guests_not_member"] = guests_not_member
-    serializer = BookingSerializer(data=data)
-    assert not serializer.is_valid()
-
-
-@pytest.mark.django_db
-@pytest.mark.parametrize(
-    "departure_date",
-    [
-        (str(now().date() + timedelta(days=15))),
-        (str(now().date() + timedelta(days=17))),
-        (str(now().date() + timedelta(days=20))),
-    ],
-)
-def test_check_date_outside_booking_window_raises_error(data, departure_date):
-    data["departure_date"] = departure_date
-    serializer = BookingSerializer(data=data)
-    assert not serializer.is_valid()
-
-
-@pytest.mark.django_db
 @pytest.mark.parametrize("key_status", [("picked_up"), ("not_picked_up"), ("delivered")])
-def test_validate_key_status(data, key_status):
-    pass
+def test_validate_key_status_lets_through_valid(data, key_status):
+    data["key_status"] = key_status
+    serializer = BookingSitSerializer(data=data)
+    assert serializer.is_valid()
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize("key_status", [("picked_upasdf"), ("not_pickqwefup"), ("delivereqwed")])
+def test_validate_key_status_picks_up_invalid(data, key_status):
+    data["key_status"] = key_status
+    try:
+        serializer = BookingSitSerializer(data=data)
+    except Exception:
+        assert not serializer.is_valid()
